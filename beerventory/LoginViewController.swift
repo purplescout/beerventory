21478a94549a5d09b4a4b68551c1f37415d84416
Mia Henriksson <mia.henriksson@gmail.com>
2015-01-27 20:54:17 +0100

    Add login view

---
+++ beerventory/LoginViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LoginViewController.swift
+//  beerventory
+//
+//  Created by Mia Henriksson on 2015-01-27.
+//  Copyright (c) 2015 Önders et Gonas. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+  @IBOutlet weak var emailTextField: UITextField!
+  @IBOutlet weak var passwordTextField: UITextField!
+  @IBOutlet weak var nameTextField: UITextField!
+  @IBOutlet weak var invitationCodeTextField: UITextField!
+
+}
